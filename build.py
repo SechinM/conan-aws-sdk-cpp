9e943615cc20b8e976d2c6e1f2d105e5397e4635
m.sechin <m.sechin@avantize.com>
2020-12-03 12:04:03 +0200

    Updated travis.yml

--- build.py
+++ build.py
@@ -3,5 +3,6 @@ from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
-    builder.add_common_builds(shared_option_name="aws-sdk-cpp:shared")
+    builder.add(settings={'compiler.libcxx': 'libstdc++11', 'arch': 'x86_64',
+        'build_type': 'Release'})
     builder.run()
